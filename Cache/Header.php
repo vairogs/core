d03b1b43ef22ee543fae615362d7538ddf14bc18
k0d3r1s <k0d3r1s@gmail.com>
2022-04-19 05:28:33 +0300

    retire cache-utils

---
+++ Cache/Header.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types = 1);
+
+namespace Vairogs\Core\Cache;
+
+final class Header
+{
+    final public const CACHE_VAR = 'Vairogs-Cache';
+    final public const INVALIDATE = 'invalidate';
+    final public const SKIP = 'skip';
+}
